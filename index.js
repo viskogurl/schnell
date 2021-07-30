6f38ac318e54f0896977ed26a8db65844f30b2ff
Ian King <ianking741@gmail.com>
2021-07-30 12:25:52 -0500

    Singleton implementation.

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
 'use strict';
 
-const Schnell = {
-  memory: [],
+const Schnell = (() => {
+  const memory = [];
 
-  tryify: async (promise) => {
+  const snailify = async (promise) => {
     try {
       const data = await promise;
       return [data, null];
     } catch (error) {
       return [null, error];
     }
-  },
+  }
 
-  clean: async () => {
-    for (const item of memory) {
-      memory.pop();
-    }
-  },
+  return {
+    clean: async () => {
+      for (const item of memory) {
+        memory.pop();
+      }
+    },
 
-  delete: async () => {},
+    delete: async() => {
 
-  all: async (model) => {
-    return memory[0] ? [memory, null] : await tryify(model.find({}));
-  },
+    },
 
-  cache: async (promise) => {
-    const [data, error] = await tryify(promise);
-    for (const item of data) {
-      memory.push(item);
-    }
-    return memory;
-  },
+    all: async () => {
+      return memory[0] ? [memory, null] : await snailify(Todo.find({}));;
+    },
 
-  find: async (snail, model) => {
-    const result = memory.find(({ id }) => id === snail);
-    return result
-      ? [result, null]
-      : await tryify(model.findOne({ id: snail }).exec());
-  },
+    cache: async (promise) => {
+      const [data, error] = await snailify(promise);
+      for (const item of data) {
+        memory.push(item);
+      }
+      return memory;
+    },
 
-  save: async (snail, model) => {
-    const [data, error] = await tryify(model.create(snail));
-    memory.push(snail);
-    return [data, error, size];
-  },
-};
+    find: async (query) => {
+      const result = memory.find(({ id }) => id === query);
+      return result ? [result, null] : await snailify(Todo.findOne({ id: query }).exec());
+    },
+
+    // Change to where properties aren't hardcoded.
+    save: async (obj) => {
+      const { color, contents } = obj;
+      const [data, error] = await snailify(Todo.create({ todoID: uuidv4(), color, contents }));
+      memory.push(obj);
+      console.log(JSON.stringify(memory).length / 1000 / 1000);
+      return [data, error];
+    },
+  };
+})();
 
 module.exports = Object.freeze(Schnell);
